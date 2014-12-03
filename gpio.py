6a3bbc5578a66fde5e429a1117992231c6711a27
Jacob.King <Jacob.King@hootsuite.com>
2014-12-03 15:24:21 -0800

    updating gpio.py w/args

---
+++ gpio.py
@@ -0,0 +1,23 @@
+import sys, re, optparse
+
+first_re = re.compile(r'^\d{3}$')
+
+parser = optparse.OptionParser()
+parser.set_defaults(debug=False,xls=False)
+parser.add_option('--debug', action='store_true', dest='debug')
+parser.add_option('--xls', action='store_true', dest='xls')
+(options, args) = parser.parse_args()
+
+if len(args) == 1:
+    if first_re.match(args[0]):
+        print "Primary argument is : ", args[0]
+    else:
+        raise ValueError("First argument should be ...")
+elif len(args) > 1:
+    raise ValueError("Too many command line arguments")
+
+if options.debug:
+    print 'debug flag'
+
+if options.xls:
+    print 'xls flag'
